d5f1af82ceb3fca761f847e4fafca2d509d79668
honzim <honzim@gmail.com>
2013-05-23 22:35:48 +0200

    listView.setOnItemLongClickListener

--- src/com/example/mojeapp/NewListView.java
+++ src/com/example/mojeapp/NewListView.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -43,13 +44,26 @@ public class NewListView extends Activity {
         
         listView.setOnItemClickListener(new OnItemClickListener() {
         	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        		Toast.makeText(getApplicationContext(), "Vybral jsi " + id, Toast.LENGTH_SHORT).show();
-        		Intent intent = new Intent(ctx, NewUpravitProduktActivity.class);
+        		Toast.makeText(getApplicationContext(), "Koupit " + id, Toast.LENGTH_SHORT).show();
+//        		Intent intent = new Intent(ctx, NewUpravitProduktActivity.class);
+//        		String message = String.valueOf(id);
+//        		intent.putExtra(INTENTID, message);
+//        		startActivity(intent);
+     		}
+     	});
+        
+        listView.setOnItemLongClickListener(new OnItemLongClickListener() {
+        	public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        		Toast.makeText(getApplicationContext(), "Dlouuuuze" + id, Toast.LENGTH_SHORT).show();
+				
+				Intent intent = new Intent(ctx, NewUpravitProduktActivity.class);
         		String message = String.valueOf(id);
         		intent.putExtra(INTENTID, message);
         		startActivity(intent);
-     		}
-     	});       
+				return false;
+        		
+        	}
+		});
 	}
 
 	public void onClickEnterData(View btnAdd) {
